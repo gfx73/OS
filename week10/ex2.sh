d22cd4f0735b7f58c434598ecb02edf9c5096a1e
gfx73 <65644911+gfx73@users.noreply.github.com>
2020-10-20 18:33:13 +0500

    Delete ex2.sh

--- week10/ex2.sh
+++
@@ -1,5 +0,0 @@
-touch ../week01/file.txt
-link ../week01/file.txt _ex2.txt >> ex2.txt
-find ../week01/file.txt -inum 1967389 >> ex2.txt
-find ../week01/file.txt -inum 1967389 -exec rm {} \; >> ex2.txt
-rm _ex2.txt
